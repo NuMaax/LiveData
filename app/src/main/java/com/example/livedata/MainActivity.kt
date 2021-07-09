556dd6c0ad355eee32c29662d09fe9b023b55cc7
NuMaax <numaax@gmail.com>
2021-07-09 15:35:57 +0300

    done

--- app/src/main/java/com/example/livedata/MainActivity.kt
+++ app/src/main/java/com/example/livedata/MainActivity.kt
@@ -1,13 +1,40 @@
+/*
+https://www.youtube.com/watch?v=ASWrCDBLfE4
+*/
 package com.example.livedata
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import com.example.livedata.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val myLiveData = MyLiveData()
+    private lateinit var observer : Observer<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        observer = Observer {
+            binding.tvTextViewMain.text = it
+        }
 
+        binding.btnGo.setOnClickListener {
+            myLiveData.setChangeLiveData(binding.etAnyValue.text.toString())
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        myLiveData.observe(this, observer)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        myLiveData.removeObserver(observer)
     }
 }
